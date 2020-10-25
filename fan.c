eec8a765952ff08b99f2c9e042ed30e9fc994893
dstmar <dstmar@users.noreply.github.com>
2020-10-25 15:45:52 -0400

    Update fan.c
    Add host setting instead of using localhost, and cleanup types used

--- fan.c
+++ fan.c
@@ -5,42 +5,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
-const int port = 8089;     /* Port used by wpi server */
+const char* host = "127.0.0.1";  /* wpi server */
+const uint16_t port = 8089;      /* Port used by wpi server */
 
 /* Print error message and exit */
-void error(char *message)
+int error(char *message)
 {
   printf("Error : %s\n", message);
-  exit(1);
+  return 1;
 }
 
 int main (void)
 {
-  struct sockaddr_in serv_addr;
-  int addr_size = sizeof(serv_addr);
- 
   printf("Content-Type:text/plain\n\n");
 
   int sock = socket(AF_INET, SOCK_DGRAM, 0);
   if (sock < 0)
-    error("Create socket");
+    return error("Create socket");
+  
+  struct sockaddr_in serv_addr;
+  int addr_size = sizeof(serv_addr);
+
   serv_addr.sin_family = AF_INET;
-  serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+  serv_addr.sin_addr.s_addr = inet_addr(host);
   serv_addr.sin_port = htons(port);
   
-  char msg[256] = "fan.cgi";
-  char buffer[256];
-
+  char* msg = "fan.cgi";
   sendto(sock, msg, strlen(msg), 0, (struct sockaddr*) &serv_addr, addr_size);
-  int n = recvfrom(sock, buffer, 255, 0, (struct sockaddr *) &serv_addr, &addr_size);
-  if (n < 0) error("Receiving message from server");
-  else
-  {
-    buffer[n] = '\0';
-    printf("%s\n", buffer);
-  }
+  
+  int buf_size = 256;
+  char buffer[buf_size];
+  int n = recvfrom(sock, buffer, buf_size-1, 0, (struct sockaddr *) &serv_addr, &addr_size);
+  if (n < 0)
+    return error("Receiving message from server");
+  
+  close(sock);
+
+  buffer[n] = '\0';
+  printf("%s\n", buffer);
+  
   return 0;
 }
